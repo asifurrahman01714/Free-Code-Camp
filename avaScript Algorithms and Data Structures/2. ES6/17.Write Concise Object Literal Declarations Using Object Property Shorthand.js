b263d9f258e8c1f6c35c86842b4741172b33d849
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-10-24 00:45:58 +0600

    Make an arrow function to create an object simply

--- avaScript Algorithms and Data Structures/2. ES6/17.Write Concise Object Literal Declarations Using Object Property Shorthand.js
+++ avaScript Algorithms and Data Structures/2. ES6/17.Write Concise Object Literal Declarations Using Object Property Shorthand.js
@@ -21,15 +21,21 @@ Use object property shorthand with object literals
 to create and return an object with name, age and gender properties.
 */
 
+/*
 const createPerson = (name, age, gender) => {
-    // Only change code below this line
-    // return {
-    //   name: name,
-    //   age: age,
-    //   gender: gender
-    // };
-    // Only change code above this line
+    return {
+      name: name,
+      age: age,
+      gender: gender
+    };
+  };
+*/
+/*
+const createPerson = (name, age, gender) => {
     return {name, age, gender};
   };
+*/
+const createPerson = (name, age, gender) => ({name, age, gender});
+
 
 console.log(createPerson("asif", "24", "male"));
