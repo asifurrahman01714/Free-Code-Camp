c4ebeaf147c73fee2f89d1cbd594367aa3f14e45
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-10-30 12:48:27 +0600

    17.Write Concise Object Literal Declarations Using Object Property Shorthand folder

--- avaScript Algorithms and Data Structures/2. ES6/29.Handle a Fulfilled Promise with then/29.Handle a Fulfilled Promise with then.js
+++ avaScript Algorithms and Data Structures/2. ES6/29.Handle a Fulfilled Promise with then/29.Handle a Fulfilled Promise with then.js
@@ -44,3 +44,6 @@ const makeServerRequest = new Promise((resolve, reject) => {
   makeServerRequest.then(result => {
     console.log(result)
   });
+  makeServerRequest.catch(error => {
+    console.log(error);
+  });
