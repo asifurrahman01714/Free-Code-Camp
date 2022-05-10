afaf26f6bef77a01611669acf2e85cddaa97df20
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-05-10 08:14:29 +0600

    Combine Arrays with the Spread Operator

--- JavaScript Algorithms and Data Structures/Basic Data Structures/9.Combine Arrays with the Spread Operator.js
+++ JavaScript Algorithms and Data Structures/Basic Data Structures/9.Combine Arrays with the Spread Operator.js
@@ -10,3 +10,12 @@ Using spread syntax, we have just achieved an operation that would have been mor
 
 We have defined a function spreadOut that returns the variable sentence. Modify the function using the spread operator so that it returns the array ['learning', 'to', 'code', 'is', 'fun'].
 */
+
+function spreadOut() {
+  let fragment = ['to', 'code'];
+  let sentence = ['learning', ...fragment,'is','fun']; // Change this line
+  return sentence;
+}
+
+const result = spreadOut();
+console.log(result)
