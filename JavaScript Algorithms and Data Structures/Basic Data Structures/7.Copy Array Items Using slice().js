59ddf2bf9807293fe1b310bd1418383f13489030
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-05-10 06:03:42 +0600

    Copy Array Items Using slice()

--- JavaScript Algorithms and Data Structures/Basic Data Structures/7.Copy Array Items Using slice().js
+++ JavaScript Algorithms and Data Structures/Basic Data Structures/7.Copy Array Items Using slice().js
@@ -10,3 +10,14 @@ In effect, we have created a new array by extracting elements from an existing a
 
 We have defined a function, forecast, that takes an array as an argument. Modify the function using slice() to extract information from the argument array and return a new array that contains the string elements warm and sunny.
 */
+
+function forecast(arr) {
+    // Only change code below this line
+    console.log(arr)
+    let newArr = arr.slice(2,4)
+    console.log(newArr)
+    return newArr;
+  }
+  
+  // Only change code above this line
+  forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms'])
